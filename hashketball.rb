939257cdb194fb723d867cefba6f1cf3e2d44c61
Liam Healy <lhealy0711@gmail.com>
2019-09-14 20:52:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -92,11 +92,13 @@ def player_numbers(team_name)
   new_hash = game_hash
   player_numbers = []
   new_hash.each do |team, players|
-    players[:players].each do |names|
-      names.each do |this_player, name|
-      # binding.pry
-      if players[:team_name] == team_name
-        player_numbers << name[:number].to_i
+    if players[:team_name] == team_name
+      players[:players].each do |names|
+        names.each do |this_player, name|
+        # binding.pry
+        if players[:team_name] == team_name
+          player_numbers << name[:number].to_i
+        end
       end
     end
   end
